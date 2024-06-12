2aab38424195601599dea33773d2974f0725b00a
莱西丶凌盾 <1098346640@qq.com>
2024-06-12 14:12:49 +0000

    补充了初始化GUI
    Signed-off-by: 莱西丶凌盾 <1098346640@qq.com>

--- init.sh
+++ init.sh
@@ -16,9 +16,11 @@ MASTER_QQ=$(dialog --no-lines --inputbox "请输入机器人主人的QQ号码：
 # 提示用户输入主要的QQ群
 MAIN_GROUP=$(dialog --no-lines --inputbox "请输入主要的QQ群：" 10 50 3>&1 1>&2 2>&3)
 # 更新config.json文件
-cat > CONFIG_FILE <<EOL
+sudo rm $CURRENT_DIR/Manyana/config.json
+cat > $CONFIG_FILE <<EOL
 {"botName": "$BOT_NAME", "botQQ": "$BOT_QQ", "master": "$MASTER_QQ","mainGroup": "$MAIN_GROUP", "vertify_key": "1234567890", "port": "23456"}
 EOL
+chmod 777 $CONFIG_FILE
 # 复制napcat.json和onebot11.json到对应的文件名
 cp "$CURRENT_DIR/NapCat/config/napcat.json" "$CURRENT_DIR/NapCat/config/napcat_$BOT_QQ.json"
 cp "$CURRENT_DIR/NapCat/config/onebot11.json" "$CURRENT_DIR/NapCat/config/onebot11_$BOT_QQ.json"
