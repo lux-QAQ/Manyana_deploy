1db1ee120aa34d0092d96bd77768bd5aaeba62a7
莱西丶凌盾 <1098346640@qq.com>
2024-06-16 03:09:35 +0000

    update gui.sh.
    Signed-off-by: 莱西丶凌盾 <1098346640@qq.com>

--- gui.sh
+++ gui.sh
@@ -2,16 +2,16 @@
 START_DIR=$(pwd)
 # 检查进程是否在运行
 check_running() {
-    screen -ls | grep -q "$1"
+    tmux list-sessions | grep -q "$1"
     if [ $? -eq 0 ]; then
-        echo "\Zb\Z2运行中\\Zn"
+        echo "\Zb\Z2运行中\Zn"
     else
         echo "\Zb\Z1未运行\Zn"
     fi
 }
 # 查看日志
 view_log() {
-    tail -n 30  "$1"
+    tail -n 30 "$1"
     sleep 3
 }
 while true; do
@@ -34,14 +34,15 @@ while true; do
     case $CHOICE in
         1)
             if [ "$NAPCAT_STATUS" == "\Zb\Z1未运行\Zn" ]; then
-                $START_DIR/1_napcat_withoutgui.sh
+                bash $START_DIR/1_napcat_withoutgui.sh
                 dialog --no-lines --colors --msgbox "\n\Zb\Z3Napcat 已启动，等待登录二维码...，请2s后按Enter键\Zn" 10 50
-                sleep 2
-                tail -n 30  $START_DIR/napcat_log.txt &
+                sleep 6
+                clear
+                cat $START_DIR/napcat_log.txt &
                 LOG_SUCCESS=0
                 end=$((SECONDS+60))
                 while [ $SECONDS -lt $end ]; do
-                    if grep -q -e "登录成功" -e "无法重复登录" $START_DIR/napcat_log.txt; then
+                    if grep -q -e "登录成功" -e "无法重复登录" "$START_DIR/napcat_log.txt"; then
                         LOG_SUCCESS=1
                         break
                     fi
@@ -51,7 +52,7 @@ while true; do
                     dialog --no-lines --colors --msgbox "\n\Zb\Z2Napcat 登录成功\Zn" 10 50
                     NAPCAT_STATUS="\Zb\Z2运行中\Zn"
                 else
-                    screen -S napcat -X quit
+                    tmux kill-session -t napcat
                     dialog --no-lines --colors --msgbox "\n\Zb\Z1超时未登录，请重新启动\Zn" 10 50
                     NAPCAT_STATUS="\Zb\Z1未运行\Zn"
                 fi
@@ -61,7 +62,7 @@ while true; do
             ;;
         2)
             if [ "$OVERFLOW_STATUS" == "\Zb\Z1未运行\Zn" ]; then
-                $START_DIR/2_overflow_withoutgui.sh
+                bash $START_DIR/2_overflow_withoutgui.sh
                 dialog --no-lines --colors --msgbox "\n\Zb\Z3Overflow 已启动\Zn" 10 50
             else
                 dialog --no-lines --colors --msgbox "\n\Zb\Z2Overflow 已经在运行中\Zn" 10 50
@@ -70,7 +71,7 @@ while true; do
         3)
             if [ "$MANYANA_STATUS" == "\Zb\Z1未运行\Zn" ]; then
                 sleep 10
-                $START_DIR/3_Manyana_withoutgui.sh
+                bash $START_DIR/3_Manyana_withoutgui.sh
                 dialog --no-lines --colors --msgbox "\n\Zb\Z3Manyana 启动中，请耐心等待，你可以退出脚本，重进脚本以检查是否启动成功\Zn" 10 50
             else
                 dialog --no-lines --colors --msgbox "\n\Zb\Z2Manyana 已经在运行中\Zn" 10 50
@@ -89,9 +90,9 @@ while true; do
             view_log "$START_DIR/manyana_log.txt"
             ;;
         7)
-            screen -S napcat -X quit
-            screen -S overflow -X quit
-            screen -S Manyana -X quit
+            tmux kill-session -t napcat
+            tmux kill-session -t overflow
+            tmux kill-session -t Manyana
             dialog --no-lines --colors --msgbox "\n\Zb\Z1所有进程已停止\Zn" 10 50
             ;;
         8)
