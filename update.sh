25e204efcd0048ed64765caff3ac7ad0a1df1ada
莱西丶凌盾 <1098346640@qq.com>
2024-06-16 09:15:07 +0000

    update update.sh.
    Signed-off-by: 莱西丶凌盾 <1098346640@qq.com>

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+CURRENT_DIR=$(pwd)/..
 cd $CURRENT_DIR/Manyana/
 source $HOME/miniconda/bin/activate qqbot
-python setUp.py | tee $START_DIR/update_log.txt
+python setUp.py | tee $CURRENT_DIR/start/update_log.txt
