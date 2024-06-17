7baec28a8441f2e5cfedcf80ef32c1464d95c134
莱西丶凌盾 <1098346640@qq.com>
2024-06-17 07:19:42 +0000

    update update.sh.
    Signed-off-by: 莱西丶凌盾 <1098346640@qq.com>

--- update.sh
+++ update.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
-CURRENT_DIR=$(pwd)/..
+# 尝试解析符号链接获取实际路径
+if command -v readlink &> /dev/null; then
+    script_path=$(readlink -f "$0")
+elif command -v realpath &> /dev/null; then
+    script_path=$(realpath "$0")
+else
+    # 不支持 readlink 或 realpath 的情况
+    script_path=$(cd "$(dirname "$0")"; pwd -P)/$(basename "$0")
+fi
+
+START_DIR=$(dirname "$script_path")
+CURRENT_DIR=$(cd "$script_dir/.."; pwd)
 clear
 cd $CURRENT_DIR/Manyana/
 source $HOME/miniconda/bin/activate qqbot
