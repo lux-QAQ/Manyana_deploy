cd6e02f03f8462e55770f69923478166c2cc4cde
莱西丶凌盾 <1098346640@qq.com>
2024-06-17 07:18:42 +0000

    update gui.sh. 修改了路径获取方式
    Signed-off-by: 莱西丶凌盾 <1098346640@qq.com>

--- gui.sh
+++ gui.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-START_DIR=$(pwd)
-CURRENT_DIR=$(pwd)/..
+# 尝试解析符号链接获取实际路径
+if command -v readlink &> /dev/null; then
+    script_path=$(readlink -f "$0")
+elif command -v realpath &> /dev/null; then
+    script_path=$(realpath "$0")
+else
+    # 不支持 readlink 或 realpath 的情况
+    script_path=$(cd "$(dirname "$0")"; pwd -P)/$(basename "$0")
+fi
+
+START_DIR=$(dirname "$script_path")
+CURRENT_DIR=$(cd "$script_dir/.."; pwd)
 # 检查是否存在 onebot_xxxx.json 文件
 check_init() {
     if ls $CURRENT_DIR/NapCat/config/onebot11_*.json 1> /dev/null 2>&1; then
